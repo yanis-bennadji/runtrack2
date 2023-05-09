7c0c1689dda4e76356ca85c56e48add505db6df5
yanis-bennadji <115157293+yanis-bennadji@users.noreply.github.com>
2023-05-09 12:18:01 +0200

    Push du job 01.

---
+++ jour01/job01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $str = "LaPlateforme";
+    $str2 = "Vive";
+    $str3 = "!";
+
+    echo "$str2 $str $str3<br>";
+
+    $val = 6;
+    echo "$val<br>";
+
+    $val = $val + 4;
+    echo "$val<br>";
+
+    $myBool = true;
+    echo "$myBool<br>";
+
+    $myBool = false;
+    echo "$myBool<br>";
+
+    ?>
+</body>
+</html>
